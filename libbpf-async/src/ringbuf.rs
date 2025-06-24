1e683aad474a1afab76d04cc7d0a36339965314b
hongrp <hongrp@mail2.sysu.edu.cn>
2025-06-24 15:58:33 +0000

    two stage Async RingBuffer construction, allow support sync environment

--- libbpf-async/src/ringbuf.rs
+++ libbpf-async/src/ringbuf.rs
@@ -15,18 +15,20 @@ const BPF_RINGBUF_BUSY_BIT: u32 = 1 << 31;
 const BPF_RINGBUF_DISCARD_BIT: u32 = 1 << 30;
 const BPF_RINGBUF_HDR_SZ: u32 = 8;
 
-pub struct RingBuffer {
+/// A synchronous version of RingBuffer that can be created outside of tokio runtime
+pub struct SyncRingBuffer {
     mask: u64,
-    async_fd: AsyncFd<RawFd>,
+    fd: RawFd,
     consumer: NonNull<core::ffi::c_void>,
     producer: NonNull<core::ffi::c_void>,
     data: NonNull<core::ffi::c_void>,
 }
 
-unsafe impl Send for RingBuffer {}
-unsafe impl Sync for RingBuffer {}
+unsafe impl Send for SyncRingBuffer {}
+unsafe impl Sync for SyncRingBuffer {}
 
-impl RingBuffer {
+impl SyncRingBuffer {
+    /// Create a new SyncRingBuffer from a Map
     pub fn new(map: &Map) -> Self {
         let max_entries = map.max_entries();
         let psize = page_size::get();
@@ -60,29 +62,82 @@ impl RingBuffer {
         let producer = unsafe { NonNull::new_unchecked(producer) };
         let data = unsafe { NonNull::new_unchecked(producer.as_ptr().add(psize)) };
 
-        RingBuffer {
+        SyncRingBuffer {
             mask: (max_entries - 1) as u64,
-            async_fd: AsyncFd::with_interest(fd, tokio::io::Interest::READABLE).unwrap(),
+            fd,
             consumer,
             producer,
             data,
         }
     }
 
-    fn roundup_len(mut len: u32) -> u32 {
-        len <<= 2;
-        len >>= 2;
-        len += BPF_RINGBUF_HDR_SZ;
-        (len + 7) / 8 * 8
+    /// Convert this SyncRingBuffer into an async RingBuffer
+    /// This must be called within a tokio runtime
+    pub fn into_async(self) -> Result<RingBuffer> {
+        let async_fd = AsyncFd::with_interest(self.fd, tokio::io::Interest::READABLE)?;
+        
+        Ok(RingBuffer {
+            mask: self.mask,
+            async_fd,
+            consumer: self.consumer,
+            producer: self.producer,
+            data: self.data,
+            // Don't drop the original SyncRingBuffer fields
+            _sync_rb: None,
+        })
     }
 }
 
-impl Drop for RingBuffer {
+impl Drop for SyncRingBuffer {
     fn drop(&mut self) {
         let psize = page_size::get();
         unsafe {
             let _ = nix::sys::mman::munmap(self.consumer.as_ptr(), psize);
             let _ = nix::sys::mman::munmap(self.producer.as_ptr(), psize + 2 * (self.mask as usize + 1));
+        }
+    }
+}
+
+/// The async version of RingBuffer that can be used with tokio
+pub struct RingBuffer {
+    mask: u64,
+    async_fd: AsyncFd<RawFd>,
+    consumer: NonNull<core::ffi::c_void>,
+    producer: NonNull<core::ffi::c_void>,
+    data: NonNull<core::ffi::c_void>,
+    // This field is used to prevent double-free when RingBuffer is created from SyncRingBuffer
+    _sync_rb: Option<SyncRingBuffer>,
+}
+
+unsafe impl Send for RingBuffer {}
+unsafe impl Sync for RingBuffer {}
+
+/// Helper function for rounding up the length
+fn roundup_len(mut len: u32) -> u32 {
+    len <<= 2;
+    len >>= 2;
+    len += BPF_RINGBUF_HDR_SZ;
+    (len + 7) / 8 * 8
+}
+
+impl RingBuffer {
+    /// Create a new RingBuffer from a Map
+    /// This must be called within a tokio runtime
+    pub fn new(map: &Map) -> Self {
+        let sync_rb = SyncRingBuffer::new(map);
+        sync_rb.into_async().unwrap()
+    }
+}
+
+impl Drop for RingBuffer {
+    fn drop(&mut self) {
+        // Only free memory if this RingBuffer wasn't created from a SyncRingBuffer
+        if self._sync_rb.is_none() {
+            let psize = page_size::get();
+            unsafe {
+                let _ = nix::sys::mman::munmap(self.consumer.as_ptr(), psize);
+                let _ = nix::sys::mman::munmap(self.producer.as_ptr(), psize + 2 * (self.mask as usize + 1));
+            }
         }
     }
 }
@@ -106,7 +161,7 @@ impl AsyncRead for RingBuffer {
                 std::sync::atomic::fence(std::sync::atomic::Ordering::SeqCst);
 
                 if (len & BPF_RINGBUF_BUSY_BIT) == 0 {
-                    cons_pos += RingBuffer::roundup_len(len) as u64;
+                    cons_pos += roundup_len(len) as u64;
                     if (len & BPF_RINGBUF_DISCARD_BIT) == 0 {
                         let sample = unsafe {
                             std::slice::from_raw_parts_mut(
